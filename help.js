09ac43fa0ee8a5df63491d777fff0f5503ef567f
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-09-08 19:26:12 +0300

    Added telegram bot api

--- help.js
+++ help.js
@@ -1,10 +1,28 @@
-const ig = require('instagram-stories');
+const { getUserByUsername, getStories } = require('instagram-stories');
 require('dotenv').config()
 
 const { USER_ID: userId, SESSION_ID: sessionid } = process.env;
 
-exports.getStories = (id) => ig.getStories({ id, userId, sessionid });
-exports.getStoriesFeed = (id) => ig.getStoriesFeed({ id, userId, sessionid });
-exports.getMediaByCode = (code) => ig.getMediaByCode({ code, userId, sessionid });
-exports.getUserByUsername = (username) => ig.getUserByUsername({ username, userId, sessionid });
-exports.getMediaByLocation = (id) => ig.getMediaByLocation({ id, userId, sessionid });
+class Instagram {
+    static async getStoriesByUsername(username) {
+        const { user } = await getUserByUsername({ username, userId, sessionid });
+        const { items: stories } = await getStories({ id: user.id, userId, sessionid });
+
+
+        return stories.map(story => {
+            if (story.video_versions) {
+                return {
+                    type: 'video',
+                    url: story.video_versions[0].url
+                }
+            } else if (story.image_versions2) {
+                return {
+                    type: 'image',
+                    url: story.image_versions2.candidates[0].url
+                }
+            }
+        })
+    }
+}
+
+module.exports = Instagram;
--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('Get a stories from instagram');
+const TelegramBot = require('node-telegram-bot-api');
+const Instagram = require('./help');
+require('dotenv').config();
+
+const { TELEGRAM_TOKEN } = process.env;
+
+const bot = new TelegramBot(TELEGRAM_TOKEN, { polling: true });
+
+bot.on('message', async (msg) => {
+    const { text, chat: { id: chatId } } = msg;
+
+    try {
+        const loadingMessage = await bot.sendMessage(chatId, 'loading...');
+        const stories = await Instagram.getStoriesByUsername(text);
+
+        bot.deleteMessage(chatId, loadingMessage.message_id);
+
+        if (stories.length === 0) {
+            bot.sendMessage(chatId, `The user ${text} has no stories`);
+            return;
+        }
+
+        stories.forEach(story => {
+            if (story.type === 'video') {
+                bot.sendVideo(msg.chat.id, story.url)
+            } else if (story.type === 'image') {
+                bot.sendPhoto(msg.chat.id, story.url);
+            }
+        })
+    } catch (error) {
+        btn.sendMessage(chatId, `Error when getting a story from ${text}`);
+    }
+});
